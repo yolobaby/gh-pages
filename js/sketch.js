9f022863480f48504788d73f179cf9a5751f4cf6
yolobaby <35843499+yolobaby@users.noreply.github.com>
2018-04-10 22:49:25 -0400

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,59 +1,104 @@
-// **** Global Variables ***** //
+// Global Variables //
 var apiKey = 'b48851c57ea2d35039a425fa9cedaa26';
-var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var weatherData;
-var city= 'New York';
-var weatherDataForecast;
+var city = 'New York';
 var button;
 var cityInput;
-var description='';
+var description = '';
 var temperature = 0;
+var tempMax = 0;
+var tempMin = 0;
 var humidity = 0;
-var speed = 0;
-var sunrise = 0;
-var sunset = 0;
 var pressure = 0;
 
-// **** Preload Function **** //
-function preload(){
-  var request = baseURL + 'New York' + '&apikey=' + apiKey;
-  weatherData = loadJSON(request);
-  print(weatherData);
-}
+
 
 // **** Setup Function ****** //
 function setup(){
-  createCanvas(800, 800);
-  background(200);
+
+  createCanvas(800, 550);
+
+  textFont ('Roboto Condensed');
+  textSize (14);
+  button = select('#submit');
+  cityInput = select('#city');
+  button.mousePressed(queryAPI);
 
   noLoop();
-  button= select('#submit');
-  button.mousePressed(queryAPI);
 }
 
-// queryAPI//
-function queryAPI (){
-  var request = baseURL+city+ '&apikey='+apikey;
+// **** Query API Function *** //
+function queryAPI(){
+  var request = baseURL + cityInput.value() + '&apikey=' + apiKey;
   loadJSON(request, getWeatherData);
 }
-function getWeatherData(apiResponse){
-  weatherData = apiResponse;
-  redraw();
 
+function getWeatherData(apiData){
+  weatherData = apiData;
+  description = weatherData.weather[0].main;
+  temperature = weatherData.main.temp;
+  tempMax = weatherData.main.temp_max;
+  tempMin = weatherData.main.temp_min;
+  humidity = weatherData.main.humidity;
+  wind = weatherData.wind.speed;
+  print(weatherData);
+  redraw();
 }
 
+
+
+
+// **** Draw Function **** //
 function draw(){
+
+  var roundTemp = Math.round((temperature * (9/5)) - 459.67);
+  var roundMax = Math.round((tempMax * (9/5)) - 459.67);
+  var roundMin = Math.round((tempMin * (9/5)) - 459.67);
+
+  background(255);
+
+      if (roundTemp < 100){
+	background(255)
+	noStroke();
+  fill(240, 213, 205);
+  rect(400, 50, 800, 800);
+   }
+
+
+
+
   if (weatherData){
 
 
-  var description = weatherData.weather[0].description;
-  var temperature = weatherData.main.temp;
-  var humidity = weatherData.main.humidity;
-  var pressure = weatherData.main.pressure;
-  text('The current weather for New York City is:', 50, 50);
-  text(description, 80, 70);
-  text(temperature + ' F', 80, 90);
-  text(humidity + '% humidity', 80, 110);
-  text(pressure + ' hPa (pressure)', 80, 130);
-}
+	fill(255);
+	textAlign(CENTER);
+	textSize(80);
+textFont('Roboto');
+    text(description, 650, 350);
+
+	textStyle (NORMAL);
+	textFont('Roboto');
+	textSize(50);
+	textAlign(CENTER);
+	textSize(60);
+
+  line(512.5, 230,25,25);
+  fill(108,164,240);
+  ellipse(512.5, 230,25,25);
+  ellipse(512.5, 430,25,25);
+	rect(500, 230,25,200);
+	textSize(15);
+  textAlign(RIGHT);
+	text(roundMin + ' F ', 520, 200);
+  fill(0);
+  textFont('Roboto');
+  text(humidity + '% humidity', 660, 400);
+  text(pressure + ' hPa (pressure)', 670, 420);
+
+
+  }
+
+
+
 }
